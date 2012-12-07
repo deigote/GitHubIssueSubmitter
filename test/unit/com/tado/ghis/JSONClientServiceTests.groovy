26a3bfcf978e44e060c8c4ec9b549335253631b2
deigote <diego@toharia.com>
2012-12-07 09:17:04 +0100

    Added test folder, DDT instead of TDD :-)

---
+++ test/unit/com/tado/ghis/JSONClientServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.tado.ghis
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(JSONClientService)
+class JSONClientServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
---
+++ test/unit/es/tado/ghis/FormsTagLibTests.groovy
@@ -0,0 +1,17 @@
+package es.tado.ghis
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(FormsTagLib)
+class FormsTagLibTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
